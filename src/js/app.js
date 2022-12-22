a846fc46c2e5a5dd73a6ec86f0d48e000bbb85e4
brayanospina2005 <ospinabrayan431@gmail.com>
2022-12-22 15:54:37 -0500

    section agg

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+
+function imgSlider(anything) {
+    document.querySelector('.starbucks').src = anything;
+}
+
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle')
+    circle.style.background= color;
+}
